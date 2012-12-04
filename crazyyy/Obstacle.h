40b0c705ed2602d2655dc099cc21f6b1ad5fbfcd
gkielwasser <gkielwasser@epsi.fr>
2012-12-04 18:49:25 +0100

    correction erreur dans Obstacle.h

--- crazyyy/Obstacle.h
+++ crazyyy/Obstacle.h
@@ -6,7 +6,7 @@ class Obstacle : public Cube {
 		public:
 			Obstacle();
 			Obstacle(const Obstacle &);
-			Obstacle(int x, int y, int z, double xmap, double ymap, double zmap,int h, int l, Color* c, GLuint t) : Cube(x, y, z, xmap,ymap,zmap, h, l, c, t) {};
+			Obstacle(int x, int y, int z, double xmap, double ymap, double zmap,int h, int l, Color* c, GLuint t[5]) : Cube(x, y, z, xmap,ymap,zmap, h, l, c, t) {};
 			~Obstacle();
 };
 
