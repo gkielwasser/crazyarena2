14eb3caf2b9eb33352a83bee1c132ba384ffdd8e
gkielwasser <gkielwasser@epsi.fr>
2012-12-10 11:31:44 +0100

    position

--- crazyyy/Position.h
+++ crazyyy/Position.h
@@ -2,61 +2,47 @@
 #define POSITION_H_INCLUDED
 
 class Position {
-		protected:
-			double x;
-			double y;
-			double z;
+protected:
+	float x;
+	float y;
+	float z;
 
-		public:
-			Position();
-			Position(const Position &);
+public:
+	Position();
+	Position(const Position &);
 
-			Position(double x, double y, double z) {
-				this->x = x;
-				this->y = y;
-				this->z = z;
-			};
+	Position(float x, float y, float z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
 
-			~Position();
 
-			double getX() const;
-			double getY() const;
-			double getZ() const;
-			void setX(double x);
-			void setY(double y);
-			void setZ(double z);
+	~Position();
+
+	float getX() {
+		return x;
+	}
+
+	float getY() {
+		return y;
+	}
+
+	float getZ() {
+		return z;
+	}
+
+	void setX(float x) {
+		this->x = x;
+	}
+
+	void setY(float y) {
+		this->y = y;
+	}
+
+	void setZ(float z) {
+		this->z = z;
+	}
 };
-
-double Position::getX() const
-{
-    return x;
-}
-
-double Position::getY() const
-{
-    return y;
-}
-
-double Position::getZ() const
-{
-    return z;
-}
-
-void Position::setX(double x)
-{
-    this->x = x;
-}
-
-void Position::setY(double y)
-{
-    this->y = y;
-}
-
-void Position::setZ(double z)
-{
-    this->z = z;
-}
-
-
 
 #endif // POSITION_H_INCLUDED
