76c2489fd05dc938c1d9518328a415122802f25f
gkielwasser <gkielwasser@epsi.fr>
2012-12-10 11:29:12 +0100

    bonus

--- crazyyy/Bonus.h
+++ crazyyy/Bonus.h
@@ -5,16 +5,17 @@ class Bonus {
 		public:
 			   Bonus(); // Non impl�ment� donc inutilisable
 			   Bonus(const Bonus &); // Constructeur de copie, non impl�ment� donc interdit
-			   Bonus(int x, int y, int a, int speed, int type);
+			   Bonus(float x, float y, float z, float speed, int type);
 	           ~Bonus(); // Destructeur
-	           int x,y,a,speed,type;
+	           float x,y,z,speed
+	           int type;
 	           void Draw();
 	           void Position();
-	           double getX();
+	           float getX();
 
-	           double getY();
+	           float getY();
 
-	           double getA();
+	           float getA();
 	           int getType();
 
 
