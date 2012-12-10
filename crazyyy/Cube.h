dfc5363f09b0c91736a7b75854d73b0c93618b41
gkielwasser <gkielwasser@epsi.fr>
2012-12-10 11:29:44 +0100

    cube

--- crazyyy/Cube.h
+++ crazyyy/Cube.h
@@ -1,18 +1,26 @@
 #ifndef CUBE_H_INCLUDED
 #define CUBE_H_INCLUDED
-#include "sdlglutils.h"
-#include "Color.h"
 
+#include "Color.h"
+#include "Plate.h"
+
+class Plate;
 class Cube {
+
 public:
-	double x;
-	double y;
-	double z;
-	double h;
-	double l;
-	double xmap;
-	double ymap;
-	double zmap;
+	Plate* plate;
+	int x;
+	int y;
+	int z;
+	int h;
+	int l;
+	int xmap;
+	int ymap;
+	int zmap;
+	int xcol;
+	int ycol;
+	int zcol;
+
 
 	/*
 	 * Textures
@@ -21,8 +29,7 @@ public:
 	GLuint* textures;
 	Color* color;
 
-	Cube(double x, double y, double z, double h, double l, double xmap,
-			double ymap, double zmap, Color* color, GLuint* textures) {
+	Cube(int x, int y, int z, int h, int l, int xmap, int ymap, int zmap, Color* color, GLuint* textures,Plate* plate) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
@@ -33,120 +40,46 @@ public:
 		this->zmap = zmap;
 		this->color = color;
 		this->textures = textures;
+		this->xcol = xmap - 1;
+		this->plate = plate;
 	}
 
 	~Cube();
 
 	//Utiliser la fontion glScale() pour agrandir réduire le cube
-	void draw() {
-		glPushMatrix();
-		glTranslated(this->x, this->y, this->z);
-
-
-		glColor3ub(51, 51, 51);
-
-		glBindTexture(GL_TEXTURE_2D, this->textures[0]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(0, 0, 0);
-		glTexCoord2d(1, 1);
-		glVertex3d(1, 0, 0);
-		glTexCoord2d(1, 0);
-		glVertex3d(1, 1, 0);
-		glTexCoord2d(0, 0);
-		glVertex3d(0, 1, 0);
-		glEnd();
-
-		glColor3ub(51, 51, 51);
-		glBindTexture(GL_TEXTURE_2D, this->textures[1]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(0, 0, 0);
-		glTexCoord2d(1, 1);
-		glVertex3d(1, 0, 0);
-		glTexCoord2d(1, 0);
-		glVertex3d(1, 0, 1);
-		glTexCoord2d(0, 0);
-		glVertex3d(0, 0, 1);
-		glEnd();
-
-		glColor3ub(51, 51, 51);
-		glBindTexture(GL_TEXTURE_2D, this->textures[2]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(1, 0, 0);
-		glTexCoord2d(1, 1);
-		glVertex3d(1, 1, 0);
-		glTexCoord2d(1, 0);
-		glVertex3d(1, 0, 1);
-		glTexCoord2d(0, 0);
-		glVertex3d(1, 0, 1);
-		glEnd();
-
-		glColor3ub(this->color->getRed(), this->color->getGreen(), this->color->getBlue());
-		glBindTexture(GL_TEXTURE_2D, this->textures[3]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(1, 1, 0);
-		glTexCoord2d(1, 1);
-		glVertex3d(0, 1, 0);
-		glTexCoord2d(1, 0);
-		glVertex3d(0, 1, 1);
-		glTexCoord2d(0, 0);
-		glVertex3d(1, 1, 1);
-		glEnd();
-
-		glColor3ub(51, 51, 51);
-		glBindTexture(GL_TEXTURE_2D, this->textures[4]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(0, 0, 0);
-		glTexCoord2d(1, 1);
-		glVertex3d(0, 1, 0);
-		glTexCoord2d(1, 0);
-		glVertex3d(0, 1, 1);
-		glTexCoord2d(0, 0);
-		glVertex3d(0, 0, 1);
-		glEnd();
-
-		glColor3ub(51, 51, 51);
-		glBindTexture(GL_TEXTURE_2D, this->textures[5]);
-		glBegin(GL_QUADS);
-		glTexCoord2d(0, 1);
-		glVertex3d(0, 0, 1);
-		glTexCoord2d(1, 1);
-		glVertex3d(1, 0, 1);
-		glTexCoord2d(1, 0);
-		glVertex3d(1, 1, 1);
-		glTexCoord2d(0, 0);
-		glVertex3d(0, 1, 1);
-		glEnd();
-
-		glPopMatrix();
+	virtual void draw() {
 	}
 
-	double getX() {
+	int getX() {
 		return this->x;
 	}
 
-	double getY() {
+	int getY() {
 		return this->y;
 	}
 
-	double getZ() {
+	int getZ() {
 		return this->z;
 	}
 
-	double getXMap() {
+	int getXMap() {
 		return this->xmap;
 	}
 
-	double getYMap() {
+	int getYMap() {
 		return this->ymap;
 	}
 
-	double getZMap() {
+	int getZMap() {
 		return this->zmap;
+	}
+
+	int getXCol() {
+		return this->xcol;
+	}
+
+	Plate* getPlate(){
+		return this->plate;
 	}
 };
 
