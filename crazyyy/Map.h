d77f19c98a1274ed632b1abb8aefe72e205afee5
gkielwasser <gkielwasser@epsi.fr>
2012-12-10 11:30:30 +0100

    map

--- crazyyy/Map.h
+++ crazyyy/Map.h
@@ -6,41 +6,62 @@
 using namespace std;
 
 class Map {
-	protected:
-		vector<Plate*> plates;
+protected:
+	vector<Plate*> plates;
+	int far;
+	int near;
 
-	public:
-		   Map();
-		   Map(const Map &);
+public:
+	Map();
+	Map(const Map &);
 
-		   Map(vector<Plate*> plates) {
-			   this->plates = plates;
-		   }
+	Map(vector<Plate*> plates) {
+		this->plates = plates;
+		this->far = 28;
+		this->near = -4;
+	}
 
-		   ~Map();
+	~Map();
 
-		   void draw() {
-			   // Pour chaque plaque
-				for (unsigned int i = 0; i < plates.size(); i++) {
-					plates[i]->draw();
+	void draw(float x, float y, float z) {
+		// Pour chaque plaque
+		for (unsigned int i = 0; i < plates.size(); i++) {
+			if(plates[i]->getX() > int(x - far)  && plates[i]->getX() < int(x + far)  && plates[i]->getZ() >= int(z)+ near  && plates[i]->getZ() < int(z + far) ) plates[i]->draw();
+		}
+	}
+
+	Cube* getCube(int x, int y, int z) {
+		Cube* cube = 0;
+
+		for (unsigned int i = 0; i < plates.size(); i++) {
+			vector<Cube*> cubes = this->plates[i]->getCubes();
+
+			for (unsigned int j = 0; j < cubes.size(); j++) {
+				if (cubes[j]->getXMap() == x && cubes[j]->getYMap() == y && cubes[j]->getZMap() == z) {
+					cube = cubes[j];
 				}
-		   }
+			}
+		}
 
-		   Cube* getCube(int x, int y, int z) {
-			   Cube* cube = 0;
+		return cube;
+	}
+	;
+	Cube* getCubeCol(int x, int y, int z) {
+		Cube* cube = 0;
 
-			   for (unsigned int i = 0; i < plates.size(); i++) {
-				   vector<Cube*> cubes = this->plates[i]->getCubes();
+		for (unsigned int i = 0; i < plates.size(); i++) {
+			vector<Cube*> cubes = this->plates[i]->getCubes();
 
-				   for (unsigned int j = 0; j < cubes.size(); j++) {
-					   if (cubes[j]->getXMap() == x && cubes[j]->getYMap() == y && cubes[j]->getZMap() == z) {
-						   cube = cubes[j];
-					   }
-				   }
-			   }
+			for (unsigned int j = 0; j < cubes.size(); j++) {
+				if (cubes[j]->getXCol() == x && cubes[j]->getYMap() == y && cubes[j]->getZMap() == z) {
+					cube = cubes[j];
+				}
+			}
+		}
 
-			   return cube;
-		   };
+		return cube;
+	}
+	;
 };
 
 #endif // Map_H_INCLUDED
